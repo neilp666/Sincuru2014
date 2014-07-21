f05116e45360111607fdeed3cf7df0bdf4d56c0b
neil patel <neil@myindianweddingapp.com>
2014-07-21 17:07:07 +0100

    finish setup for Dropbox

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,22 +1,12 @@
-class Listing < ActiveRecord::
 
-
-	if Rails.env.development?
-
-			has_attached_file :image, :styles => { :small => "50x", :thumb => "100x100>" }, :default_url => "default.jpg"
-			validates_attachment_content_type :image, content_type: %w(image/jpeg image/jpg image/png), 
-
-	else
-
-		 has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "missing.jpg",
-                          :storage => :dropbox,
-                          :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
-                          :path => ":style/id_:filename"
-
+class Listing < ActiveRecord::Base
+  if Rails.env.development?
+    has_attached_file :image, :styles => { :small => "50x", :thumb => "100x100>" }, :default_url => "default.jpg"
+    validates_attachment_content_type :image, :content_type => %w(image/jpeg image/jpg image/png)
+  else
+    has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg",
+        :storage => :dropbox,
+        :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+        :path => ":style/:id_:filename"
+  end
 end
-
-
-validates_attachment_content_type :image, :content_type => %w(image/jpeg image/jpg image/png) 
-  validates :name, :description, :price, presence: true
-  validates :price, numericality: { greater_than: 0 }
-  validates_attachment_presence :image
