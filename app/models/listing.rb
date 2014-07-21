1af3c3f19617255d0cd6e7e8632f9ddc6db5a8f9
neil patel <neil@myindianweddingapp.com>
2014-07-21 17:16:02 +0100

    Updated the listings.rb file

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,10 +3,13 @@ class Listing < ActiveRecord::Base
   if Rails.env.development?
     has_attached_file :image, :styles => { :small => "50x", :thumb => "100x100>" }, :default_url => "default.jpg"
     validates_attachment_content_type :image, :content_type => %w(image/jpeg image/jpg image/png)
+
   else
     has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg",
         :storage => :dropbox,
         :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
         :path => ":style/:id_:filename"
   end
+  validates_attachment_content_type :image, :content_type => %w(image/jpeg image/jpg image/png)
+  validates_attachment_presence :image
 end
