17efff1f0f8e93e248778cf10956050ab0eebcda
neil patel <neil@myindianweddingapp.com>
2014-07-22 14:46:39 +0100

    Associate listings with users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140722114555) do
+ActiveRecord::Schema.define(version: 20140722131911) do
 
   create_table "listings", force: true do |t|
     t.string   "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20140722114555) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
